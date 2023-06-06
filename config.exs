63e519525192b774e098d3a4ec5c481feb925087
EdgarVeider <guimatos.com@gmail.com>
2023-06-06 19:37:04 -0300

    atualizacao mangalivre

---
+++ config.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :ex_cldr,
+  default_locale: {"pt", "BR"}
--- lib/scrap.ex
+++ lib/scrap.ex
@@ -1,13 +1,40 @@
 defmodule Scrap do
 
-  def pegar_tokens(string) do
 
-    result = String.split(string, "</h1>")
+  def pegar_tokens(string, rank \\ 1) do
 
-    [head | _tail] = result
+    [head | tail] = string
 
-    IO.inspect(head)
+    manga_link = "https://mangalivre.net" <> head
 
+    case HTTPoison.get(manga_link) do
+      {:ok, %HTTPoison.Response{body: body}} ->
+        content = body
+        head = Floki.find(content, "head")
+        tile_element = Floki.find(head ,"title")
+        title = Floki.text(tile_element)
+
+        IO.puts("\n")
+        string_title = "##{rank}: #{title}"
+        IO.inspect(string_title)
+
+        desc_element = Floki.find(head, "meta[name='description']")
+        desc = Floki.attribute(desc_element, "content")
+        descricao = Floki.text(desc)
+        string_desc = "Descricao: #{descricao}"
+        IO.inspect(string_desc)
+
+        body = Floki.find(content, "div.series-info")
+        span = Floki.find(body, "span.series-author")
+
+        dado = Enum.at(span, 1)
+        list_span = Tuple.to_list(dado)
+        IO.inspect(Enum.at(list_span, 2))
+
+        IO.puts("\n")
+
+
+    end
 
 
   end
@@ -17,16 +44,13 @@ defmodule Scrap do
     case HTTPoison.get("https://mangalivre.net/lista-de-mangas/ordenar-por-numero-de-leituras/todos/desde-o-comeco") do
       {:ok, %HTTPoison.Response{body: body}} ->
 
-        elem = Floki.find(body, "div.seriesList")
-        list = Floki.find(elem, "li")
-        title = Floki.find(list, "span.series-title")
-        header = Floki.find(title, "h1")
+        div = Floki.find(body, "div.seriesList")
+        ul = Floki.find(div, "ul.seriesList")
+        anchor = Floki.find(ul, "a.link-block")
+        links = Floki.attribute(anchor, "href")
 
-        text = Floki.raw_html(header)
 
-        str = to_string(text)
-
-        pegar_tokens(str)
+        pegar_tokens(links)
 
     end
   end
