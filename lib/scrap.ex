279517ce28f83ff840aed45745ae046f090c9b04
EdgarVeider <guimatos.com@gmail.com>
2023-06-09 16:18:20 -0300

    Erro de finalização corrigido

--- lib/scrap.ex
+++ lib/scrap.ex
@@ -1,53 +1,55 @@
 defmodule Scrap do
 
 
-
   def pegar_tokens(string, rank \\ 1) do
 
+    case (length(string) == 0) do
+      true ->
+        IO.puts("Fim")
+      false ->
+        [head | tail] = string
 
-    if !string do
-      IO.puts("FIM DA RECURSAO")
+
+        manga_link = "https://mangalivre.net" <> head
+
+        case HTTPoison.get(manga_link) do
+          {:ok, %HTTPoison.Response{body: body}} ->
+            content = body
+            head = Floki.find(content, "head")
+            tile_element = Floki.find(head ,"title")
+            title_raw = Floki.text(tile_element)
+
+            [title | _trash] = (String.split(title_raw, "(pt-BR)"))
+
+            string_title = "##{rank}: #{title}"
+
+            desc_element = Floki.find(head, "meta[name='description']")
+            desc = Floki.attribute(desc_element, "content")
+            descricao = Floki.text(desc)
+            string_desc = "Descricao: #{descricao}"
+
+            series_info = Floki.find(content, "div.series-info")
+            span = Floki.find(series_info, "span.series-author")
+            dado = Enum.at(span, 1)
+            author_raw = String.trim(String.replace(Floki.text(dado), "\n", ""))
+
+            score_div = Floki.find(body, "div.score-number")
+            score = String.trim(Enum.at(String.split(Floki.text(score_div), "\n"), 1))
+
+
+            IO.puts("\n")
+            IO.inspect(string_title)
+            IO.inspect(string_desc)
+            IO.inspect("Author: #{author_raw}")
+            IO.inspect("Nota: #{score}")
+            IO.puts("\n")
+
+          end
+
+
+        pegar_tokens(tail, (rank + 1))
     end
 
-    [head | tail] = string
-
-
-    manga_link = "https://mangalivre.net" <> head
-
-    case HTTPoison.get(manga_link) do
-      {:ok, %HTTPoison.Response{body: body}} ->
-        content = body
-        head = Floki.find(content, "head")
-        tile_element = Floki.find(head ,"title")
-        title_raw = Floki.text(tile_element)
-
-        [title | _trash] = (String.split(title_raw, "(pt-BR)"))
-
-        string_title = "##{rank}: #{title}"
-
-        desc_element = Floki.find(head, "meta[name='description']")
-        desc = Floki.attribute(desc_element, "content")
-        descricao = Floki.text(desc)
-        string_desc = "Descricao: #{descricao}"
-
-        series_info = Floki.find(content, "div.series-info")
-        span = Floki.find(series_info, "span.series-author")
-        dado = Enum.at(span, 1)
-        author_raw = String.trim(String.replace(Floki.text(dado), "\n", ""))
-
-        score_div = Floki.find(body, "div.score-number")
-        score = String.trim(Enum.at(String.split(Floki.text(score_div), "\n"), 1))
-
-
-        IO.puts("\n")
-        IO.inspect(string_title)
-        IO.inspect(string_desc)
-        IO.inspect("Author: #{author_raw}")
-        IO.inspect("Nota: #{score}")
-        IO.puts("\n")
-
-      pegar_tokens(tail, (rank + 1))
-    end
 
 
 end
@@ -64,6 +66,7 @@ end
 
 
         pegar_tokens(links)
+        IO.puts("CHAMADA FINAL")
 
     end
   end
